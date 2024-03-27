75a0fe8a7d37a989751593d68a7f40b2fb39f19b
Lea Albano <leaalbano@csu.fullerton.edu>
2024-03-27 15:23:07 -0700

    Added logic for profile.js

--- chat-gpt-chrome/src/component/Profile.js
+++ chat-gpt-chrome/src/component/Profile.js
@@ -2,9 +2,20 @@ import React from 'react';
 import 'nice-forms.css';
 import { IoArrowBack } from "react-icons/io5";
 import { ROUTES } from '../utils/routes';
+import { saveData } from '../utils/localStorage';
 
 
-function Profile({setPage}) {
+function Profile({setPage, resume, setResume, openAIKey, setOpenAIKey}) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const updatedResume = formData.get("resume");
+    const updatedOpenAIKey = formData.get("openAIKey");
+    setResume(updatedResume);
+    setOpenAIKey(updatedOpenAIKey);
+    saveData('resume', updatedResume);
+    saveData('openAIKey', updatedOpenAIKey);
+  }
   return (
     <div className="flex flex-col  mx-5">
       <div className="flex flex-row justify-between my-3 items-center">
@@ -17,7 +28,7 @@ function Profile({setPage}) {
         </button>
       </div>
 
-      <form className="flex-col">
+      <form className="flex-col" onSubmit={handleSubmit}>
         <div className="mb-6">
           <label
             htmlFor="openAIKey"
@@ -31,6 +42,7 @@ function Profile({setPage}) {
             type="text"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="sk-...1234"
+            defaultValue={openAIKey}
             required
           />
         </div>
@@ -44,10 +56,10 @@ function Profile({setPage}) {
           <textarea
             id="resume"
             name="resume"
-            
             rows={8}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Paste your resume here..."
+            defaultValue={resume}
           ></textarea>
         </div>
         <div className="mb-6 text-center">
